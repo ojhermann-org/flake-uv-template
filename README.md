c20748a200632e02ccff823ebe1b5d2590c1fc08
Otto J. Hermann <ojhermann@gmail.com>
2026-03-24 19:32:02 -0400

    Set up (#1)

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # grokking_algorithms_second_edition_python
+
+## what
+
+A place for me to provide an idea of how I like to develop in Python
+
+## why
+
+I work with Python a lot; this will help others I might work with get a feel for
+my style
+
+## how
+
+I'm coding elements of
+[Grokking Algorithms](https://www.manning.com/books/grokking-algorithms-second-edition)
+because the familiarity of the material should allow other engineers to focus on
+how I'm using Python and related tooling
---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Grokking Algorithms (2nd Ed.) — Python";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              pkgs.prek
+              pkgs.uv
+              pkgs.nixfmt
+              pkgs.statix
+              pkgs.deadnix
+            ];
+          };
+        }
+      );
+    };
+}
